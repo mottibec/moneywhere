1457387b7a0deac130e8981aefdbab6f7775afbc
Motti Bechhofer <MBechhofer@encoresupport.org>
2019-02-24 15:03:21 -0500

    added user card

--- app/components/UserCard.js
+++ app/components/UserCard.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { Card, ListItem, Button, Icon } from 'react-native-elements'
 
 export default class UserCard extends React.Component {
-    render(){
-        return 
-        <View>
-            <Text>{this.props.user.name}</Text>
-            <Text>{this.props.user.rating}</Text>
-        </View>
+    render() {
+        return (
+            <Card>
+                <View>
+                    <Image
+                        resizeMode="cover"
+                        source={{ uri: this.props.user.avatar }}
+                    />
+                    <Text style={styles.name}>{this.props.user.name}</Text>
+                    <Text>{this.props.user.rating}</Text>
+                </View>
+            </Card>
+        )
     }
 }
+
+const styles = StyleSheet.create({
+    name: {
+
+    }
+});
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,38 +1,74 @@
 import React from 'react';
 import {
   Platform,
+  Dimensions,
   StyleSheet,
   View,
+  Text
 } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import UserCard from '../components/UserCard';
+import Geolocation from 'react-native-geolocation-service';
+
+const screen = Dimensions.get('window');
+
+const ASPECT_RATIO = screen.width / screen.height;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
-
+  initialRegion = {
+    latitude: 37.600425,
+    longitude: -122.385861,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA,
+  };
+  constructor() {
+    super();
+    this.state = {
+      users: [
+        { name: "motti", id: 1, rating: 5, avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg" },
+        { name: "nave", id: 1, rating: 2, avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg" }
+      ]
+    };
+  }
+  componentDidMount() {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+      },
+      (error) => {
+        // See error code charts below.
+        console.log(error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    );
+  }
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <MapView
           provider={PROVIDER_GOOGLE}
-          style={styles.map}>
-
+          style={styles.map}
+          initialRegion={this.initialRegion}>
         </MapView>
-        <View pointerEvents="none" style={styles.users}>
+        <View pointerEvents="none" style={styles.members}>
           {this.createUserCards()}
         </View>
       </View>
     );
   }
   createUserCards() {
-    const { users } = this.state;
+    let users = this.state.users;
+    if (users === null) {
+      return <View><Text>No Users Found</Text></View>;
+    }
     return users.map(user => {
       return (
-        <View key={user.id} style={styles.member}>
-          <UserCard user={user}></UserCard>
-        </View>
+        <UserCard user={user}></UserCard>
       );
     });
   }
@@ -40,10 +76,14 @@ export default class HomeScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  users: {
 
   }
 });
