4855b0103412e976f5f623228b00c56194094f87
Motti Bechhofer <MBechhofer@encoresupport.org>
2019-02-23 21:27:05 -0500

    init app

---
+++ app/screens/ProfileScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+export default class ProfileScreen extends React.Component {
+    render(){
+        return 
+        <View>
+            <ScrollView>
+            <Image source={this.userProfile} style={styles.profilePicure}></Image>
+            <Text>{this.rating}</Text>
+            </ScrollView>
+        </View>
+    }
+}
+const styles = StyleSheet.create({
+    profilePicure:{
+        width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+    }
+});
--- routes/transactions.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from "express";
-
-const router: Router = Router();
-
-router.get("/transactions/:userId", (request:Request, response: Response)  => {
-    response.send('Hello world!');
-});
-
-export { router };
